ba39c19c390f5d9e7409979be28731288908ac9d
irakli <iberidze38@gmail.com>
2023-10-20 23:48:35 +0400

    some changes

--- src/pages/Tankers.js
+++ src/pages/Tankers.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Tankers() {
   return (
-    <div>tankers</div>
+    <div className='decs'>
+        <div className="labels">
+          <Link to='/'>
+              <h3>განაცხადები</h3>
+          </Link>
+          <Link to='/tankers'>
+              <h3>ტანკერები</h3>
+          </Link>
+        </div>
+    </div>
   )
 }
 
