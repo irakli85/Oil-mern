97b436426872944951a8ceb264af344d23037e07
iberidze38 <iberidze38@gmail.com>
2023-11-29 11:20:51 +0400

    first commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,27 @@
 import {useState} from 'react'
 import { useLogin } from '../hooks/useLogin'
+import spin from '../assets/load.gif'
+import styled from 'styled-components'
 
 const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const {login, error, isLoading} = useLogin()
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault()
 
         await login(email, password)
     }
+
+    const handleLogin = () => {
+        setLoading(true);
+        setTimeout(() => {
+          setLoading(false);
+          console.log('Logged in!');
+        }, 2000); // Simulating a 2-second delay, replace with your actual API call or logic
+      };
 
     return(
         <form className="login" onSubmit={handleSubmit}>
@@ -21,11 +32,26 @@ const Login = () => {
 
             <label>პაროლი:</label>
             <input type="password" onChange={ (e) => setPassword(e.target.value)} value={password}/>
-
-            <button disabled={isLoading}>შესვლა</button>
-            {error && <div className='error'>{error}</div>}
+            <Div>
+                <button disabled={isLoading} onClick={handleLogin}>შესვლა</button>
+                {loading && <Img src={spin} alt="Loading..." />}
+                {error && <div className='error'>{error}</div>}
+            </Div>
         </form>
     )
 }
+
+const Div = styled.div`
+    display: flex;
+    position: relative;
+`
+
+const Img = styled.img`
+    width: 80px;
+    height: 80px;
+    position: absolute;
+    top: -20px;
+    right: 200px;
+`
 
 export default Login
